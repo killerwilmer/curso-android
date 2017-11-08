710a8cf6611f1edf48dfcc49a2e44a5277ac5c9f
Wilmer <killerwilmer@gmail.com>
2017-11-08 14:05:40 -0500

    ListViewDemo

---
+++ ListViewDemo/app/src/main/java/com/killerwilmer/listviewdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.killerwilmer.listviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+
+        final ArrayList<String> myFamily = new ArrayList<String>();
+
+        myFamily.add("Wilmer");
+        myFamily.add("Edwin");
+        myFamily.add("Florinda");
+        myFamily.add("Moises");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("Nombre: ", myFamily.get(i));
+            }
+        });
+
+    }
+}
