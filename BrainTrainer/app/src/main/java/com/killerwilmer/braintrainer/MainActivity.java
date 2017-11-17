0c42a48bd35cfdda788c740787dc50d97ef32d22
Wilmer <killerwilmer@gmail.com>
2017-11-17 11:00:54 -0500

    Init BrainTrainer

---
+++ BrainTrainer/app/src/main/java/com/killerwilmer/braintrainer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.killerwilmer.braintrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+
+    public void start(View view) {
+        startButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+    }
+}
