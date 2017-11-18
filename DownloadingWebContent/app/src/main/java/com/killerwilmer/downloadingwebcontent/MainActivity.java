30daf16ceb70e0f43e655e3ce93dac5b65141d3a
Wilmer <killerwilmer@gmail.com>
2017-11-18 12:55:09 -0500

    DownloadingWebContent + AsyncTask

---
+++ DownloadingWebContent/app/src/main/java/com/killerwilmer/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,65 @@
+package com.killerwilmer.downloadingwebcontent;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            //Log.i("URL", strings[0]);
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                int data = inputStreamReader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed";
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        try {
+            result = task.execute("https://www.ecowebhosting.co.uk/").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("Contents of URL ", result);
+    }
+}
