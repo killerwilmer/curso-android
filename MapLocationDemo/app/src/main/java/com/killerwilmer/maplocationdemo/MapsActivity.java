bc447f909306279085fcec768764765989b1a97a
Wilmer <killerwilmer@gmail.com>
2017-11-23 17:28:33 -0500

    Getting Information About Locations

--- MapLocationDemo/app/src/main/java/com/killerwilmer/maplocationdemo/MapsActivity.java
+++ MapLocationDemo/app/src/main/java/com/killerwilmer/maplocationdemo/MapsActivity.java
@@ -3,6 +3,8 @@ package com.killerwilmer.maplocationdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +23,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -63,6 +70,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.clear();
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 20));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if (addressList != null && addressList.size() > 0) {
+                        Log.i("PlaceInfo", addressList.get(0).toString());
+
+                        String address = "";
+
+                        if (addressList.get(0).getSubThoroughfare() != null) {
+                            address += addressList.get(0).getSubThoroughfare() + " ";
+                        }
+                        if (addressList.get(0).getThoroughfare() != null) {
+                            address += addressList.get(0).getThoroughfare() + ", ";
+                        }
+                        if (addressList.get(0).getLocality() != null) {
+                            address += addressList.get(0).getLocality() + ", ";
+                        }
+                        if (addressList.get(0).getPostalCode() != null) {
+                            address += addressList.get(0).getPostalCode() + ", ";
+                        }
+                        if (addressList.get(0).getCountryName() != null) {
+                            address += addressList.get(0).getCountryName();
+                        }
+
+                        Toast.makeText(MapsActivity.this, address, Toast.LENGTH_SHORT).show();
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
