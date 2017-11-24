995f20454816d12348f32ce439139e37e7f4b208
Wilmer <killerwilmer@gmail.com>
2017-11-24 07:48:15 -0500

    MultipleActivitiesDemo, passing values

---
+++ MultipleActivitiesDemo/app/src/main/java/com/killerwilmer/multipleactivitiesdemo/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.killerwilmer.multipleactivitiesdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public void backToMain(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+
+        Toast.makeText(this, intent.getStringExtra("username"), Toast.LENGTH_SHORT).show();
+    }
+}
