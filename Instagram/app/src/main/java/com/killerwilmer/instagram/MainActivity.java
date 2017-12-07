d4fba3a0e72edc1226593d30ff176d4f6e16090d
Wilmer <killerwilmer@gmail.com>
2017-12-07 11:12:35 -0500

    Instagram clone, Sign Up, Login, parse

---
+++ Instagram/app/src/main/java/com/killerwilmer/instagram/MainActivity.java
@@ -0,0 +1,92 @@
+package com.killerwilmer.instagram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText usernameEditText;
+    EditText passwordEditText;
+    TextView changeSignUpModeTextView;
+    Button signUpButton;
+
+    Boolean signUpModeActive = true;
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.changeSignUpModeTextView) {
+            if (signUpModeActive) {
+                signUpModeActive = false;
+                signUpButton.setText("Login");
+                changeSignUpModeTextView.setText("or, Sign Up");
+            } else {
+                signUpModeActive = true;
+                signUpButton.setText("Sign Up");
+                changeSignUpModeTextView.setText("or, Login");
+            }
+        }
+    }
+
+    public void signUp(View view) {
+
+        if (usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")) {
+            Toast.makeText(this, "A username and password are required.", Toast.LENGTH_SHORT).show();
+        } else {
+
+            if (signUpModeActive) {
+                ParseUser user = new ParseUser();
+
+                user.setUsername(usernameEditText.getText().toString());
+                user.setPassword(passwordEditText.getText().toString());
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.i("signUp", "Successful");
+                        } else {
+                            Log.i("error", e.toString());
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+                ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Log.i("Sign Up", "Login Successful");
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        changeSignUpModeTextView = (TextView) findViewById(R.id.changeSignUpModeTextView);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        changeSignUpModeTextView.setOnClickListener(this);
+    }
+}
---
+++ Instagram/app/src/main/java/com/killerwilmer/instagram/StarterApplication.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.killerwilmer.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId(getString(R.string.parse_application_id))
+            .clientKey(getString(R.string.parse_client_secret))
+            .server(getString(R.string.parse_server))
+            .build()
+    );
+
+//    ParseObject object = new ParseObject("ExampleObject");
+//    object.put("myNumber", "123");
+//    object.put("myString", "rob");
+//
+//    object.saveInBackground(new SaveCallback () {
+//      @Override
+//      public void done(ParseException ex) {
+//        if (ex == null) {
+//          Log.i("Parse Result", "Successful!");
+//        } else {
+//          Log.i("Parse Result", "Failed " + ex.toString());
+//        }
+//      }
+//    });
+
+    // For create default user at init
+    //ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
