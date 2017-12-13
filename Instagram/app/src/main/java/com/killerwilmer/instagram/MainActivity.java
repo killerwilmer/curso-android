f35303292d06ef9a8dc0a75fffc72f79f43cb5b0
Wilmer <killerwilmer@gmail.com>
2017-12-12 19:28:19 -0500

    List users from parse

--- Instagram/app/src/main/java/com/killerwilmer/instagram/MainActivity.java
+++ Instagram/app/src/main/java/com/killerwilmer/instagram/MainActivity.java
@@ -1,27 +1,38 @@
 package com.killerwilmer.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
+import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
 
     EditText usernameEditText;
     EditText passwordEditText;
     TextView changeSignUpModeTextView;
     Button signUpButton;
+    ConstraintLayout constraintLayout;
 
     Boolean signUpModeActive = true;
+
+    public void showUserList() {
+        Intent intent = new Intent(getApplicationContext(), UserListActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public void onClick(View v) {
@@ -35,6 +46,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 signUpButton.setText("Sign Up");
                 changeSignUpModeTextView.setText("or, Login");
             }
+        } else if (v.getId() == R.id.constraintLayout) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         }
     }
 
@@ -55,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void done(ParseException e) {
                         if (e == null) {
                             Log.i("signUp", "Successful");
+                            showUserList();
                         } else {
                             Log.i("error", e.toString());
                             Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
@@ -67,6 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void done(ParseUser user, ParseException e) {
                         if (user != null) {
                             Log.i("Sign Up", "Login Successful");
+                            showUserList();
                         } else {
                             Toast.makeText(MainActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
                         }
@@ -86,7 +102,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         passwordEditText = (EditText) findViewById(R.id.passwordEditText);
         changeSignUpModeTextView = (TextView) findViewById(R.id.changeSignUpModeTextView);
         signUpButton = (Button) findViewById(R.id.signUpButton);
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+
+        constraintLayout.setOnClickListener(this);
 
         changeSignUpModeTextView.setOnClickListener(this);
+
+        passwordEditText.setOnKeyListener(this);
+
+        if (ParseUser.getCurrentUser() != null) {
+            showUserList();
+        }
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == event.ACTION_DOWN) {
+            signUp(v);
+        }
+        return false;
     }
 }
