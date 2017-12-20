102e4d28768031c15131ee1577a7c814b78e293f
Wilmer <killerwilmer@gmail.com>
2017-12-20 15:07:38 -0500

    Example admob

--- FirebaseDemoApp/app/src/main/java/com/killerwilmer/firebasedemoapp/MainActivity.java
+++ FirebaseDemoApp/app/src/main/java/com/killerwilmer/firebasedemoapp/MainActivity.java
@@ -2,37 +2,25 @@ package com.killerwilmer.firebasedemoapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+        MobileAds.initialize(getApplicationContext(), getString(R.string.banner_app_unit_id));
 
-        Map<String, String> values = new HashMap<>();
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
-        values.put("name", "wilmer");
-
-        dbRef.push().setValue(values, new DatabaseReference.CompletionListener() {
-            @Override
-            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                if (databaseError == null) {
-                    Log.i("Info", "Save successful");
-                } else {
-                    Log.i("Info", "Save failed - " + databaseError.getMessage());
-                }
-            }
-        });
     }
 }
