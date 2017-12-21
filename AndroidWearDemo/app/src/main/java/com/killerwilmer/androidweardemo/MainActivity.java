a8a60fba19332d345bf823b453a22ca07fdf0f96
Wilmer <killerwilmer@gmail.com>
2017-12-20 21:48:37 -0500

    TextView, button, toast

--- AndroidWearDemo/app/src/main/java/com/killerwilmer/androidweardemo/MainActivity.java
+++ AndroidWearDemo/app/src/main/java/com/killerwilmer/androidweardemo/MainActivity.java
@@ -2,18 +2,29 @@ package com.killerwilmer.androidweardemo;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    TextView helloTextView;
+    Button clickButton;
+
+    public void click(View view) {
+        Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        helloTextView = (TextView) findViewById(R.id.helloTextView);
+        clickButton = (Button) findViewById(R.id.clickButton);
+
+        helloTextView.setText("Hello Killer");
 
         // Enables Always-on
         setAmbientEnabled();
